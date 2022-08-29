884717d5e195d8fddc709f0cd94e06df5e09ce7e
makaronma <87429019+makaronma@users.noreply.github.com>
2022-08-29 22:48:14 +0800

    move githubLogo to asset folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import CommandDisplay from './components/CommandDisplay';
 import InputArea from './components/InputArea';
 import PackageSelectSection from './components/PackageSelectSection';
-
-// TODO: add a button to set same choice for all row
+import GithubLogo from './assets/GitHub-Mark-64px.png'
 // TODO: add switch terminal
-// TODO: add single package all versions display
+// TODO: add switch yarn npm
 // TODO: add latest versions display in listing
+// TODO: add single package all versions display
 
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
         <div className="bg-white p-4 text-2xl flex font-bold flex-row shadow-lg mb-4 justify-between">
           <h1>Package Selector</h1>
           <a href="https://github.com/makaronma/package-selector" target='_blank' rel="noreferrer">
-            <img src="GitHub-Mark-64px.png" className='h-10' alt="github-source" />
+            <img src={GithubLogo} className='h-10' alt="github-source" />
           </a>
         </div>
         <div className=" px-14 pt-6 pb-24">
